5440f4f3a81d57b0802a9d3c380833e25e02c96a
Jenny <jennypf03@gmail.com>
2026-04-21 00:25:09 +0200

    upgrade  pomodoro Timer: - Fokus and Break time - 4  Fokus and Break time  sessions - "You finished -Message

--- ba_studdybuddy/src/app/pages/timer/timer.component.ts
+++ ba_studdybuddy/src/app/pages/timer/timer.component.ts
@@ -8,8 +8,14 @@ import { interval } from 'rxjs';
   styleUrl: './timer.component.css'
 })
 export class TimerComponent {
-  timeLeft = 1500; // 25 Minuten
+  focusTime = 1500; // 25 Minuten
+  breakTime = 300; // 5 Minuten
+  sessions = 4;
+
+  timeLeft = this.focusTime
+  sessionsLeft = this.sessions;
   interval: any;
+  fokus =  true;
 
   get minutes() {
     return Math.floor(this.timeLeft / 60);
@@ -19,18 +25,46 @@ export class TimerComponent {
     return this.timeLeft % 60;
   }
 
+  get modus() {
+    return this.fokus ? "Fokus" : "Break";
+  }
+
+  get session(){
+  if (this.sessionsLeft === 0) {
+    return `${this.sessions}/${this.sessions}`;
+  }
+  return `${this.sessions - this.sessionsLeft + 1}/${this.sessions}`;
+  }
+
+  get finished(){
+    if (this.sessionsLeft === 0){ return "You finished ^-^"}else {return}
+  }
+
 
   start() {
-    if (!this.interval) {
+    if (this.interval) return;
+
     this.interval = setInterval(() => {
       if (this.timeLeft > 0) {
         this.timeLeft--;
       } else {
         this.stop();
+
+        if (this.sessionsLeft > 0) {
+          if (this.fokus) {
+            this.startBreak();
+          } else {
+            this.sessionsLeft--;
+            if (this.sessionsLeft > 0) {
+              this.startFocus();
+            }
+          }
+        }
       }
     }, 1000);
   }
-  }
+
+  
 
   stop() {
     clearInterval(this.interval);
@@ -39,7 +73,20 @@ export class TimerComponent {
 
   reset() {
     this.stop();
-    this.timeLeft = 1500;
+    this.fokus =  true;
+    this.sessionsLeft = this.sessions;
+    this.timeLeft = this.focusTime;
   }
- 
-}
+
+ startBreak() {
+    this.fokus = false;
+    this.timeLeft = this.breakTime;
+    this.start();
+  }
+
+  startFocus() {
+    this.fokus = true;
+    this.timeLeft = this.focusTime;
+    this.start();
+  }
+  }
